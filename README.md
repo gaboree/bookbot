e943738d613f4a9c46adf3cfaddc35fe3cb5f397
gaboree <gaborzsolt2007@yahoo.com>
2024-04-26 12:40:53 +0300

    Charachter count report implemented, updated read.me

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # bookbot
 Bookbot is my first git boot.dev project!
+
+Script that counts number of words in a document, and number of occurence, in a decreasing list, of each alphabetic character.
+
+For use the def_path variable must be set with the absolute path of the input document.
+
+Finally execute the main.py with the pyuthon interpreter in your CLI.
--- main.py
+++ main.py
@@ -1,19 +1,53 @@
 def main():
+    #update path for other inputs
     def_path = "books/frankenstein.txt"
     words = load_book_text(def_path)
     num__words = word_counter(words)
-    print(num__words)
+    num_letters = letter_counter(words)
+    print(f"------ BEGIN REPORT OF {def_path}------")
+    print(f"Number of words in this book: {num__words}")
+    gen_report(num_letters)
+    print(f"------END OF REPORT------")
 
 def load_book_text(path):
     with open(path) as f:
         return f.read()
 
+#function for counting words
 def word_counter(in_string):
     counter = 0
+    #split by whitespaces
     words = in_string.split()
     for word in words:
         if word:
             counter += 1
     return counter
+
+#function to count letters
+def letter_counter(in_string):
+    lowered_string = in_string.lower()
+    dict_num_letters = {}
+    for i in lowered_string:
+        if i in dict_num_letters.keys():
+            dict_num_letters[i] = dict_num_letters.get(i) + 1
+        else:
+            dict_num_letters[i] = 1
+    return dict_num_letters
+
+#function for sort by number
+def sort_on(dict):
+    return dict["num"]
+
+#function to sort charachters from max to min
+def gen_report(in_dict):
+    report_letters = []
+    #create list of dicts and sort it
+    for item in in_dict.keys():
+        if item.isalpha():
+            report_letters.append({"char":item, "num":in_dict[item]})
+    report_letters.sort(reverse=True, key=sort_on)
+    #print it fancy
+    for i in range(len(report_letters)):
+        print(f"The '{report_letters[i].get("char")}' character was found {report_letters[i].get("num")} times")
 
 main()
